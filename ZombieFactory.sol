9e5ed5fcfd2c4067c3e3300d74b82c3d13374fe0
Tiffany <Owhyze1@gmail.com>
2021-05-08 23:50:07 -0400

    Creating ZombieFactory from crypto zombies io

---
+++ ZombieFactory.sol
@@ -0,0 +1,28 @@
+pragma solidity >=0.5.0 <0.6.0;
+
+contract ZombieFactory {
+  uint dnaDigits = 16;
+  uint dnaModulus = 10 ** dnaDigits;
+
+  // event definition
+
+  struct Zombie {
+    string name;
+    uint dna;
+  }
+
+  Zombie[] public zombies;
+
+  function _createZombie(string memory _name, uint _dna) private {
+    zombies.push(Zombie(_name,_dna));
+
+    // event emitter
+  }
+
+  function _generateZombieDna(string memory _str) private view returns (uint) {
+    // use hash abi...
+  }
+
+
+
+}
